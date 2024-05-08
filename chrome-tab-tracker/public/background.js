ca343225e9c07ae8aca5d37b0af29002727ed56c
Nikhil <nikhilarora10b@gmail.com>
2024-05-08 22:49:09 +0530

    created extension

---
+++ chrome-tab-tracker/public/background.js
@@ -0,0 +1,36 @@
+/* global chrome */
+
+let activeTabId = null;
+let tabTimes = {};
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+  activeTabId = activeInfo.tabId;
+  if (!tabTimes[activeTabId]) {
+    tabTimes[activeTabId] = 0;
+  }
+});
+
+chrome.windows.onFocusChanged.addListener(windowId => {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    activeTabId = null;
+  } else {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      activeTabId = tabs[0].id;
+      if (!tabTimes[activeTabId]) {
+        tabTimes[activeTabId] = 0;
+      }
+    });
+  }
+});
+
+setInterval(() => {
+  if (activeTabId) {
+    tabTimes[activeTabId]++;
+  }
+}, 1000);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.cmd === 'getTabTimes') {
+    sendResponse(tabTimes);
+  }
+});
